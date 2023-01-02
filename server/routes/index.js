b7267288244a43e91fec426b03f30a42acfa9beb
Ihor <ngorekk@gmail.com>
2023-01-02 09:37:18 +0100

    upd_back

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,10 +4,11 @@ const deviceRouter = require('./deviceRouter')
 const brandRouter = require('./brandRouter')
 const userRouter = require('./userRouter')
 const typeRouter = require('./typeRouter')
+const checkRole = require('../middleware/checkRoleMiddleware')
 
 router.use('/user', userRouter)
 router.use('/type', typeRouter)
-router.use('/brand', brandRouter)
-router.use('/device', deviceRouter)
+router.use('/brand', checkRole('ADMIN'), brandRouter)
+router.use('/device', checkRole('ADMIN'), deviceRouter)
 
 module.exports = router
